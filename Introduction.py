82d3bfdf9668ca6a204ed0bdb748b1078d08c6d7
NeloRain <nelorain@gmail.com>
2022-01-31 12:41:34 +0000

    test

---
+++ Introduction.py
@@ -0,0 +1,9 @@
+#print("hello world")
+#a = input("what is your first name?")
+#b = input("what is your second name?")
+#print("Hello", a, b)
+
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("please enter the second number: "))
+answer = num1 + num2
+print(num1, "+", num2, "=", answer)
