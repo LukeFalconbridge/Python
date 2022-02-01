2e2b1c132ebcdbc18b7e12cf4793afb674def06b
NeloRain <nelorain@gmail.com>
2022-02-01 14:29:00 +0000

    test

--- Introduction.py
+++ Introduction.py
@@ -3,7 +3,82 @@
 #b = input("what is your second name?")
 #print("Hello", a, b)
 
-num1 = float(input("Please enter the first number: "))
-num2 = float(input("please enter the second number: "))
-answer = num1 + num2
-print(num1, "+", num2, "=", answer)
+#num1 = float(input("Please enter the first number: "))
+#num2 = float(input("please enter the second number: "))
+#answer = num1 + num2
+#print(num1, "+", num2, "=", answer)
+
+#num1 = input("Enter whole number:")
+#num2 = input("Enter decimal number: ")
+#integer_number = int(num1)
+#float_number = float(num2)
+#round_number = int(round(float_number))
+#print(num1)
+#print(num2)
+#print(round_number)
+
+#pet
+#name = "Pep Gaurdogoila"
+#age = 3
+#bark = True
+#tweet = False
+#print("my per is called", name, ", he is", age, "years old")
+#print("Statement:",name, "barks", bark)
+#print("Statement", name, "tweets", tweet)
+
+#books = {"The Handmaiden's Tale":"Margaret Atwood", "The Hobbit":"J.R.R. Tolkien", "Charlie and the Chocolate Factory":"Roald Dahl"}
+#print(books)
+#print(books["The Handmaiden's Tale"])
+
+#var1 = int(input("please enter the result:"))
+
+#if var1 >= 65:
+#    if var1 >= 85:
+#        print("distinction")
+#    else:
+#        print("pass")
+#else:
+#        print("fail")
+
+# var1 = int(input("please enter the result:"))
+
+# if var1 >= 85:
+#     print("distinction")
+# elif var1 >= 65:
+#     print("pass")
+# else:
+#     print("fail")
+
+#count = 0
+# name = str(input("what is your name?"))
+#while count < 5:
+    #print(name, "is awesome!")
+    #count += 1
+
+# name = []
+# while len(name) < 5:
+#     name.append(str(input("please enter a name: ")))
+# else:
+#     for i in name:
+#         print(i," is awesome")
+
+# num = int(input("please enter a number between 1500 and 2700:"))
+# if num >= 1500 and num <= 2700:
+#     if num%5 == 0:
+#         print(num,"is divisable by 5")
+#     else:
+#         print(num,"is not divisable by 5")
+#     if num%7 == 0:
+#             print(num,"is divisable by 7")
+#     else:
+#             print(num,"is not divisable by 7")
+# else:
+#     print("try again")
+
+for n in range (1500 2700):
+    if n%5 == 0:
+        print(n,"divisable by 5")
+    if n%7 == 0:
+            print(n,"divisable by 7")
+else:
+    print("none")
