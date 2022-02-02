a1ceeb41907b9175b33ca30ba12758d0f136082c
NeloRain <nelorain@gmail.com>
2022-02-02 14:55:57 +0000

    meh

---
+++ more functions.py
@@ -0,0 +1,42 @@
+#Write a Python function to find the Max of three numbers.
+
+# num1 = int(input("please enter the first number: "))
+# num2 = int(input("please enter the second number: "))
+# num3 = int(input("please enter the third number: "))
+
+# def findmax(num1, num2, num3):
+#     list = [num1, num2, num3]
+#     top = max(list)
+#     return top
+
+# A = findmax(num1, num2, num3)
+# print(A,"is the highest number")
+
+#Write a Python function to sum all the numbers in a list
+
+# num = []
+# while len(num) < 5:
+#     num.append(int(input("please enter a number: ")))
+
+# def calc(num):
+#     result = sum(num)
+#     return result
+
+# res = calc(num)
+# print(num,"=",res)
+
+#Write a Python function to multiply all the numbers in a list.
+
+# num = []
+# while len(num) < 5:
+#     num.append(int(input("please enter a number: ")))
+
+# def calc(num):
+#     result = 1
+#     for n in num:
+#         result = result * n
+#     return result
+
+# res = calc(num)
+# print(res)
+
